eb59b8ee5c639a0c4d1ccb65f4669c154238e7ab
赵日天 <truenine304520@gmail.com>
2025-06-17 22:31:01 +0800

    🚧 [user] 用户命令监听器

--- application/src/main/kotlin/net/yan100/life/application/eventhandlers/UserEventHandlers.kt
+++ application/src/main/kotlin/net/yan100/life/application/eventhandlers/UserEventHandlers.kt
@@ -1,52 +1,52 @@
 package net.yan100.life.application.eventhandlers
 
+import net.yan100.compose.slf4j
 import net.yan100.life.domain.user.*
-import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class UserEventHandlers {
 
-  private val logger = LoggerFactory.getLogger(javaClass)
+  private val log = slf4j<UserEventHandlers>()
 
   @EventListener
   suspend fun handle(event: UserAccountCreatedEvent) {
-    logger.info("用户账户创建: userId=${event.aggregateId}, account=${event.account}")
+    log.info("[handle] user account created userId: {} , account: {}", event.aggregateId, event.account)
     // TODO: 发送欢迎邮件或短信
     // TODO: 初始化用户配置
   }
 
   @EventListener
   suspend fun handle(event: UserProfileUpdatedEvent) {
-    logger.info("用户资料更新: userId=${event.aggregateId}")
+    log.info("[handle] user profile updated userId: {}", event.aggregateId)
     // TODO: 同步更新缓存
     // TODO: 通知相关服务
   }
 
   @EventListener
   suspend fun handle(event: WechatBoundEvent) {
-    logger.info("微信绑定成功: userId=${event.aggregateId}, openId=${event.openId}")
+    log.info("[handle] wechat bound userId: {} , openId: {}", event.aggregateId, event.openId)
     // TODO: 同步微信用户信息
   }
 
   @EventListener
   suspend fun handle(event: PasswordChangedEvent) {
-    logger.info("密码已修改: userId=${event.aggregateId}")
+    log.info("[handle] password changed userId: {}", event.aggregateId)
     // TODO: 发送密码修改通知
     // TODO: 强制其他登录设备下线
   }
 
   @EventListener
   suspend fun handle(event: PostFavoritedEvent) {
-    logger.info("帖子收藏: userId=${event.aggregateId}, postId=${event.postId}")
+    log.info("[handle] post favorited userId: {} , postId: {}", event.aggregateId, event.postId)
     // TODO: 更新帖子收藏计数
     // TODO: 通知帖子作者
   }
 
   @EventListener
   suspend fun handle(event: PostUnfavoritedEvent) {
-    logger.info("取消收藏: userId=${event.aggregateId}, postId=${event.postId}")
+    log.info("[handle] post unfavorited userId: {} , postId: {}", event.aggregateId, event.postId)
     // TODO: 更新帖子收藏计数
   }
 } 
